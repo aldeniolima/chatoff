fd6e7e607dde0d8bb1c23c34107665503855b3f2
aldenio <aldeniopl@gmail.com>
2018-04-16 19:53:41 -0300

    Commit inicial.

---
+++ app/src/main/java/chatoff/pdm/tads/ifpe/edu/br/chatoff/MainActivity.java
@@ -0,0 +1,13 @@
+package chatoff.pdm.tads.ifpe.edu.br.chatoff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
