d05791db5c11d2fb25939ea933d29fa0a58ca0b3
Your Name <n.santos1897@gmail.com>
2018-04-30 20:17:01 -0300

    Inicializada atividade principal de menu

--- app/src/main/java/chatoff/pdm/tads/ifpe/edu/br/chatoff/CadastroActivity.java
+++ app/src/main/java/chatoff/pdm/tads/ifpe/edu/br/chatoff/CadastroActivity.java
@@ -3,11 +3,11 @@ package chatoff.pdm.tads.ifpe.edu.br.chatoff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
     }
 }
