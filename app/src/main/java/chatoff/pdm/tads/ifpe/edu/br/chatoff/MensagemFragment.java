a1af9c7f142f232887379e1576d7955db9014ac2
aldenio <aldeniopl@gmail.com>
2018-05-06 21:04:47 -0300

    inserido fragment lista de mensagens ao activity_main iniciado tela de chat

--- app/src/main/java/chatoff/pdm/tads/ifpe/edu/br/chatoff/MensagemFragment.java
+++ app/src/main/java/chatoff/pdm/tads/ifpe/edu/br/chatoff/MensagemFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ConversaFragment.OnFragmentInteractionListener} interface
+ * {@link MensagemFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link ConversaFragment#newInstance} factory method to
+ * Use the {@link MensagemFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ConversaFragment extends Fragment {
+public class MensagemFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class ConversaFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public ConversaFragment() {
+    public MensagemFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class ConversaFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Conversa.
+     * @return A new instance of fragment MensagemFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ConversaFragment newInstance(String param1, String param2) {
-        ConversaFragment fragment = new ConversaFragment();
+    public static MensagemFragment newInstance(String param1, String param2) {
+        MensagemFragment fragment = new MensagemFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class ConversaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conversa, container, false);
+        return inflater.inflate(R.layout.fragment_mensagem, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -76,13 +76,13 @@ public class ConversaFragment extends Fragment {
 
     @Override
     public void onAttach(Context context) {
-       super.onAttach(context);
-     /*    if (context instanceof OnFragmentInteractionListener) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
-        }*/
+        }
     }
 
     @Override
